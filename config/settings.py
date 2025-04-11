dcf92dc73d1e747972a532d60232b3b413b2be04
darreneastland <36100870+darreneastland@users.noreply.github.com>
2025-04-11 05:54:01 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,8 @@
+import os
+
+class settings:
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY") or "your-openai-key"
+    PINECONE_API_KEY = os.getenv("PINECONE_API_KEY") or "your-pinecone-key"
+    PINECONE_ENV = os.getenv("PINECONE_ENV") or "your-env"
+    PINECONE_INDEX_NAME = "dt-knowledge"
 
